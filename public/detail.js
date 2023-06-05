137dfaf6d91c22bc5b86ee0892316d5fb40460f3
Viet Duc <ducxjt18@gmai.com>
2023-06-05 19:19:49 +0700

    abcde

---
+++ public/detail.js
@@ -0,0 +1,34 @@
+const thumbnail = new Swiper(".thumbnail-carousel", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 5,
+  spaceBetween: 7,
+  watchSlidesProgress: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 5,
+      direction: "horizontal",
+    },
+    768: {
+      slidesPerView: 4,
+      direction: "horizontal",
+    },
+    992: {
+      slidesPerView: 4,
+      direction: "vertical",
+    },
+  },
+});
+
+const largeImg = new Swiper(".main-carousel", {
+  // Optional parameters
+  direction: "horizontal",
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: thumbnail,
+  },
+});
